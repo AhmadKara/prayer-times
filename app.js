8c2188536e33a65d5e036fa2b6a09e539a5d6372
Ahmad <86618117+AhmadKara@users.noreply.github.com>
2023-07-01 11:42:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
-const today = document.getElementById('today') 
-const fajr = document.getElementById('fajr') 
-const sunrise = document.getElementById('sunrise') 
-const dhuhr = document.getElementById('dhuhr') 
-const asr = document.getElementById('asr') 
-const maghrib = document.getElementById('maghrib') 
-const isha = document.getElementById('isha') 
+const today = document.getElementById('today');
+const fajr = document.getElementById('fajr');
+const sunrise = document.getElementById('sunrise');
+const dhuhr = document.getElementById('dhuhr');
+const asr = document.getElementById('asr');
+const maghrib = document.getElementById('maghrib');
+const isha = document.getElementById('isha');
+const citiesForm = document.getElementById('cities');
+const cityName = document.getElementById('city-name');
 
+const cities = ["مكة المكرمة", "المدينة المنورة", "دمشق", "أبو ظبي", "دبي"];
 
-window.onload = async function () {
-  await fetch("https://api.aladhan.com/v1/timingsByCity?country=SA&city=Makkah")
+for (let city of cities) {
+  const option = `<option>${city}</option>`;
+  citiesForm.innerHTML += option;
+}
+
+let country = "SA";
+let city = "Makkah";
+
+citiesForm.addEventListener("change", function() {
+  cityName.innerHTML = this.value;
+  switch (this.value) {
+    case "المدينة المنورة":
+      city = "Madinah";
+      country = "SA";
+      break;
+    case "دمشق":
+      city = "Damascus"
+      country = "SY";
+      break;
+    case "أبو ظبي":
+      city = "Abu Dhabi"
+      country = "AE";
+      break;
+    case "دبي":
+      city = "Dubai"
+      country = "AE";
+      break;
+    default:
+      city = "Makkah";
+      country = "SA";
+      break;
+  }
+  getDataFromAPI();
+})
+
+getDataFromAPI();
+
+function getDataFromAPI() {
+  fetch(`https://api.aladhan.com/v1/timingsByCity?country=${country}&city=${city}`)
   .then(async (res) => {
     return await res.json();
   })
@@ -17,7 +57,7 @@ window.onload = async function () {
     return times
   })
   .then(async (data) => {
-    console.log(data)
+    // console.log(data)
     const day = data.date.hijri.weekday.ar
     const fullDate = data.date.gregorian.date
     today.innerHTML = `${day} ${fullDate}`
@@ -28,4 +68,4 @@ window.onload = async function () {
     maghrib.innerHTML = await data.timings.Maghrib;
     isha.innerHTML = await data.timings.Isha;
   })
-}
+};
